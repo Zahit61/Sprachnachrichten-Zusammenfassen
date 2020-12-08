c1672588feab19f71edcabe820b8e9abb94dfb19
Zahit61 <68902420+Zahit61@users.noreply.github.com>
2020-12-08 17:14:15 +0100

    Add files via upload

---
+++ TCP-Server.py
@@ -0,0 +1,58 @@
+import socket 
+import threading
+
+HEADER = 64
+PORT = 8080
+SERVER = "127.0.0.1"
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    #conn.close()
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while (True):
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print("Hey Test")
+        #filename = input(str("Please enter a filename for the incoming file : "))
+        file = open("text.rtf", 'wb')
+        print("Teil1")
+
+        file_data = server.recv(1024)
+        print("Teil2")
+
+        file.write(file_data)
+        print("Teil3")
+
+        file.close()
+        print("File has been received successfully.")
+
+print("[STARTING] server is starting...")
+start()
+        
+
+        
