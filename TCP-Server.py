5cfe82be93755c948a6b33c1b6212eb35dfb2f51
Zahit61 <68902420+Zahit61@users.noreply.github.com>
2021-01-11 15:21:28 +0100

    Add files via upload

--- TCP-Server.py
+++ TCP-Server.py
@@ -1,6 +1,6 @@
 import socket 
 import threading
-#from oggwav import convert_ogg_to_wav
+from Speechtext import speechtext as st
 
 HEADER = 64
 PORT = 8080
@@ -10,61 +10,25 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
-    conn.close()
-        
+server.bind(ADDR)        
 
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
-    while (True):
-        conn, addr = server.accept()
-        #conn, addr = server.accept()
-        #thread = threading.Thread(target=handle_client, args=(conn, addr))
-        #thread.start()
-        #print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-        #print("Hey Test")
-        #filename = input(str("Please enter a filename for the incoming file : "))
-        
-        """file = open("test.ogg", 'ab')
-        #print("Teil1")
+    
+    conn, addr = server.accept()
+    with open('test.ogg','wb') as f:
+        print(addr)
+        while True:
+            l = conn.recv(1024)
+            if not l: break
+            f.write(l)
+    conn.close()
 
-        file_data = conn.recv(24000).decode(hex)
-        #print(file_data)
-        conn.send("Msg received".encode(FORMAT))
-
-        file.write(file_data)
-        #print("Teil3")
-
-        file.close()
-        #print("File has been received successfully.")"""
-
-        with open('test.ogg','wb') as f:
-            while True:
-                l = conn.recv(1024)
-                if not l: break
-                f.write(l)
-        conn.close()
-
-
-        
-#print("[STARTING] server is starting...")
+print("[STARTING] server is starting...")
 start()
 
-#convert_ogg_to_wav()
+print("Läuft...")
+
+text = str(st())
+print(text)
